29f6e76c549d531adaa08789d98f40cdeebf3aaf
Sanjay <attellisanjay29@gmail.com>
2025-03-24 22:40:28 +0530

    Run new project on port 8081

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Get PORT from environment
+    port = int(os.environ.get("PORT", 8081))  # Use 8081 instead of 5000
     app.run(host='0.0.0.0', port=port)
